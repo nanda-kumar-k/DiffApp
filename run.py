6a9200173d9c428467f480422ae345388f79d6f2
nanda-kumar-k <nandakumar.k0531@gmail.com>
2025-06-30 01:46:28 +0530

    [version 1.4 - run file update]

--- run.py
+++ run.py
@@ -4,10 +4,12 @@
 import os
 from app import create_app
 
+app = create_app()
+
 if __name__ == "__main__":
     # Allow FLASK_DEBUG or other env vars to control behavior
     debug_flag = os.getenv("FLASK_DEBUG", "False").lower() in ("1", "true", "yes")
-    app = create_app()
+    
     # app.run(host="0.0.0.0", port=int(os.getenv("PORT", 8000)), debug=debug_flag)
 
     app.run(host="0.0.0.0", port=int(os.getenv("PORT", 8000)), debug=True, use_reloader=True)
